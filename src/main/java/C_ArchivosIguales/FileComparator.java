facacd0675a0ad52f66fd86339472f405a763214
nquinbla <nquinbla@myuax.com>
2024-04-26 09:31:47 +0200

    [Módulo 3] código FileComparator

--- src/main/java/C_ArchivosIguales/FileComparator.java
+++ src/main/java/C_ArchivosIguales/FileComparator.java
@@ -10,16 +10,42 @@ import java.nio.file.Paths;
 public class FileComparator extends JFrame {
     private JTextField textField1;
     private JTextField textField2;
-    private JButton button;
+    private JButton button1;
+    private JButton button2;
+    private JButton compareButton;
     private JTextArea textArea;
 
     public FileComparator() {
         textField1 = new JTextField(20);
         textField2 = new JTextField(20);
-        button = new JButton("Comparar archivos");
+        button1 = new JButton("Elegir archivo 1");
+        button2 = new JButton("Elegir archivo 2");
+        compareButton = new JButton("Comparar archivos");
         textArea = new JTextArea(10, 30);
 
-        button.addActionListener(new ActionListener() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    textField1.setText(fileChooser.getSelectedFile().getPath());
+                }
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    textField2.setText(fileChooser.getSelectedFile().getPath());
+                }
+            }
+        });
+
+        compareButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String file1 = textField1.getText();
@@ -41,11 +67,13 @@ public class FileComparator extends JFrame {
         });
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        add(new JLabel("Nombre del primer archivo:"));
+        add(new JLabel("Archivo 1:"));
         add(textField1);
-        add(new JLabel("Nombre del segundo archivo:"));
+        add(button1);
+        add(new JLabel("Archivo 2:"));
         add(textField2);
-        add(button);
+        add(button2);
+        add(compareButton);
         add(new JScrollPane(textArea));
         pack();
     }
