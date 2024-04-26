e860a7189ffc4745b095926a29e449597ccbc727
nquinbla <nquinbla@myuax.com>
2024-04-26 09:44:10 +0200

    [Módulo 4] Main códgio

--- src/main/java/D_NumPalabras/Main_D.java
+++ src/main/java/D_NumPalabras/Main_D.java
@@ -1,9 +1,10 @@
 package D_NumPalabras;
 
-
 public class Main_D {
     public static void main(String[] args) {
-        ContadorPalabras contador = new ContadorPalabras();
-        contador.contarPalabras();
+        ContadorPalabras frame = new ContadorPalabras();
+        frame.setTitle("Contador de Palabras");
+        frame.setSize(400, 200);
+        frame.setVisible(true);
     }
 }
