75d0d5575b124b127aea3124aa6c215bcdd492f3
nquinbla <nquinbla@myuax.com>
2024-04-29 08:22:17 +0200

    [Módulo 5] codigo BuscaPalabras

---
+++ src/main/java/E_BuscaPalabras/BuscaPalabras.java
@@ -0,0 +1,19 @@
+package E_BuscaPalabras;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BuscaPalabras {
+    public static int countOccurrences(String filename, String word) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        String[] words = content.split("\\s+");
+        int count = 0;
+        for (String w : words) {
+            if (w.equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
