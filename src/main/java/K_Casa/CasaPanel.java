bf40cb4f2310963787972b77ecd672538dea73b8
nquinbla <nquinbla@myuax.com>
2024-04-29 08:58:10 +0200

    [Módulo 12] codigo casa pt3

--- src/main/java/K_Casa/CasaPanel.java
+++ src/main/java/K_Casa/CasaPanel.java
@@ -9,22 +9,35 @@ public class CasaPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawRect(100, 100, 200, 200);
+        g.setColor(Color.BLUE);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
-        g.drawRect(150, 250, 50, 50);
+        g.setColor(Color.RED);
+        g.fillRect(100, 100, 200, 200);
 
-        g.drawRect(220, 150, 50, 50);
+        g.setColor(Color.MAGENTA);
+        g.fillRect(150, 250, 50, 50);
 
-        g.drawLine(100, 100, 200, 50);
-        g.drawLine(200, 50, 300, 100);
+        g.setColor(Color.BLUE);
+        g.fillRect(220, 150, 50, 50);
 
-        g.setColor(Color.GREEN);
-        g.drawLine(0, 300, this.getWidth(), 300);
+        g.setColor(Color.YELLOW);
+        g.fillPolygon(new int[]{100, 200, 300}, new int[]{100, 50, 100}, 3);
+
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(0, 300, this.getWidth(), this.getHeight() - 300);
 
         g.setColor(Color.DARK_GRAY);
         g.fillRect(350, 250, 20, 50);
 
         g.setColor(Color.GREEN);
         g.fillOval(335, 200, 50, 50);
+
+        // Dibuja y colorea el sol
+        g.setColor(Color.YELLOW);
+        g.fillOval(10, 10, 50, 50);
+
+        g.setColor(Color.ORANGE);
+        g.fillRect(150, 300, 50, this.getHeight() - 300);
     }
 }
