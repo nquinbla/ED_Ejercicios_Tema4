861c170b3be8ee5b59277a12c7ef9e93d033d1c0
nquinbla <nquinbla@myuax.com>
2024-04-29 09:21:18 +0200

    Main general pt3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,112 +14,63 @@ public class Main extends JFrame {
         panel.setBackground(Color.LIGHT_GRAY);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JLabel subtitle1 = new JLabel("Nerea Quintanilla");
-        JLabel subtitle2 = new JLabel("Estructura de Datos");
+        JLabel title = new JLabel("Tema 4: Ejercicios", SwingConstants.CENTER);
+        title.setForeground(Color.BLUE); // Cambia el color del título a azul
+        panel.add(title);
+
+        JLabel subtitle1 = new JLabel("Nerea Quintanilla", SwingConstants.CENTER);
         panel.add(subtitle1);
+
+        JLabel subtitle2 = new JLabel("Estructura de Datos", SwingConstants.CENTER);
         panel.add(subtitle2);
 
         // Crea botones para llamar a los mains de los paquetes A a K
-        JButton buttonA = new JButton("Introducir carcateres");
-        buttonA.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("A_ProgramaX.Main_A");
-            }
-        });
+        JButton buttonA = createButton("Introducir carcateres", "A_ProgramaX.Main_A");
         panel.add(buttonA);
 
-        JButton buttonB = new JButton("Programa Listado");
-        buttonB.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("B_ProgramaX.Main_B");
-            }
-        });
+        JButton buttonB = createButton("Programa Listado", "B_ProgramaX.Main_B");
         panel.add(buttonB);
 
-        JButton buttonC = new JButton("Comparador Archivos");
-        buttonC.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("C_ProgramaX.Main_C");
-            }
-        });
+        JButton buttonC = createButton("Comparador Archivos", "C_ProgramaX.Main_C");
         panel.add(buttonC);
 
-        JButton buttonD = new JButton("Numero de palabras");
-        buttonD.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("D_ProgramaX.Main_D");
-            }
-        });
+        JButton buttonD = createButton("Numero de palabras", "D_ProgramaX.Main_D");
         panel.add(buttonD);
 
-        JButton buttonE = new JButton("Contador de palabras");
-        buttonE.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("E_ProgramaX.Main_E");
-            }
-        });
+        JButton buttonE = createButton("Contador de palabras", "E_ProgramaX.Main_E");
         panel.add(buttonE);
 
-        JButton buttonF = new JButton("Agenda de contactos");
-        buttonF.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("F_ProgramaX.Main_F");
-            }
-        });
+        JButton buttonF = createButton("Agenda de contactos", "F_ProgramaX.Main_F");
         panel.add(buttonF);
 
-        JButton buttonG = new JButton("Ventanas ");
-        buttonG.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("G_ProgramaX.Main_G");
-            }
-        });
+        JButton buttonG = createButton("Ventanas ", "G_ProgramaX.Main_G");
         panel.add(buttonG);
 
-        JButton buttonH = new JButton("Coordenas de un punto");
-        buttonH.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("H_ProgramaX.Main_H");
-            }
-        });
+        JButton buttonH = createButton("Coordenas de un punto", "H_ProgramaX.Main_H");
         panel.add(buttonH);
 
-        JButton buttonI = new JButton("ScrollI");
-        buttonI.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("I_ProgramaX.Main_I");
-            }
-        });
+        JButton buttonI = createButton("ScrollI", "I_ProgramaX.Main_I");
         panel.add(buttonI);
 
-        JButton buttonJ = new JButton("Validador Email");
-        buttonJ.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("J_ProgramaX.Main_J");
-            }
-        });
+        JButton buttonJ = createButton("Validador Email", "J_ProgramaX.Main_J");
         panel.add(buttonJ);
 
-        JButton buttonK = new JButton("Casa dibjada en panel");
-        buttonK.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callMain("K_ProgramaX.Main_K");
-            }
-        });
+        JButton buttonK = createButton("Casa dibjada en panel", "K_ProgramaX.Main_K");
         panel.add(buttonK);
 
         add(panel);
+    }
+
+    private JButton createButton(String text, String className) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT); // Centra el botón
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                callMain(className);
+            }
+        });
+        return button;
     }
 
     private void callMain(String className) {
