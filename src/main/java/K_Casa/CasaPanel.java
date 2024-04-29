66002774a8fd4b68a8e2b7fcd4a61568439fa0cd
nquinbla <nquinbla@myuax.com>
2024-04-29 08:56:43 +0200

    [Módulo 12] codigo casa pt2

--- src/main/java/K_Casa/CasaPanel.java
+++ src/main/java/K_Casa/CasaPanel.java
@@ -9,17 +9,22 @@ public class CasaPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Dibuja el cuerpo de la casa
         g.drawRect(100, 100, 200, 200);
 
-        // Dibuja la puerta de la casa
         g.drawRect(150, 250, 50, 50);
 
-        // Dibuja la ventana de la casa
         g.drawRect(220, 150, 50, 50);
 
-        // Dibuja el techo de la casa
         g.drawLine(100, 100, 200, 50);
         g.drawLine(200, 50, 300, 100);
+
+        g.setColor(Color.GREEN);
+        g.drawLine(0, 300, this.getWidth(), 300);
+
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(350, 250, 20, 50);
+
+        g.setColor(Color.GREEN);
+        g.fillOval(335, 200, 50, 50);
     }
 }
